788f4c1b0c35fc40c7b79f6e5d635b02b410de67
Vu Minh Diep <vuminhdiep.hycmd@gmail.com>
2021-08-21 01:11:44 +0700

    Update euler totient function.py

--- Number Theory II/euler totient function.py
+++ Number Theory II/euler totient function.py
@@ -13,18 +13,7 @@ def phi(n):
             result += 1
     return result
 
-def phi_upgrade(n):
-    result = n
-    for i in range(2, int(n ** 0.5) + 1):
-        if n % i == 0:
-            while n % i == 0:
-                n //= i
-            result = (result // i) * (i - 1)
-        if n > 1:
-            result = (result // n) * (n-1)
-        return result
 
-    
 if __name__ == "__main__":
     n = 60
     print('phi(', n, ') =', phi(n), sep= '')
