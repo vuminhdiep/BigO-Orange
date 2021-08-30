9fa48002d3f5e532789cf37ac68150fb32c1cce7
Vu Minh Diep <vuminhdiep.hycmd@gmail.com>
2021-08-31 01:35:22 +0700

    Update seperate chaining - collision for cyclic shift.py

--- Hash Table/seperate chaining - collision for cyclic shift.py
+++ Hash Table/seperate chaining - collision for cyclic shift.py
@@ -38,6 +38,7 @@ def insertKey(key):
     hashTable[index].head = newNode
     hashTable[index].count += 1
 
+
 def searchKey(key):
     index = JSHash(key) % TABLE_SIZE
     myNode = hashTable[index].head
@@ -46,3 +47,44 @@ def searchKey(key):
             return True
         myNode = myNode.next
     return False
+
+
+def deleteKey(key):
+    index = JSHash(key) % TABLE_SIZE
+    myNode = hashTable[index].head
+    if myNode == None:
+        return False
+    temp = myNode
+    while myNode != None:
+        if myNode.key == key:
+            if myNode == hashTable[index].head:
+                hashTable[index].head = myNode.next
+            else:
+                temp.next = myNode.next
+            hashTable[index].count -= 1
+            del myNode
+            return True
+        temp = myNode
+        myNode = myNode.next
+    return False
+
+def printHashTable():
+    for i in range(TABLE_SIZE):
+        if hashTable[i].count == 0:
+            continue
+        myNode = hashTable[i].head
+        if myNode == None:
+            continue
+        print('----------Index ', i, '---------', sep='')
+        j = 0
+        while myNode != None:
+            j += 1
+            print(i, '.', j, ' ', myNode.key, sep= '')
+            myNode = myNode.next
+
+if __name__ == '__main__':
+    keys = ["John", "Usha", "Summer", "Bella", "Donna", "Alice"]
+    hashTable = [HashTable() for i in range(TABLE_SIZE)]
+    for i in range(len(keys)):
+        insertKey(keys[i])
+    printHashTable()
